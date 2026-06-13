5e0808e0406ad53091d96b6478627c3b997927c0
Esther9904 <taiwoibeh@gmail.com>
2026-06-13 02:24:02 -0700

    Set futuristic design layout

--- WeatherApi.js
+++ WeatherApi.js
@@ -1,16 +1,16 @@
-async function getWeather(){
-    const city = document.getElementById("city").value;
+// async function getWeather(){
+//     const city = document.getElementById("city").value;
      
-    const apiKey = "b9bc679c2c4fd77d435ef00c21c04eed";
+//     const apiKey = "b9bc679c2c4fd77d435ef00c21c04eed";
 
-    const url = `https://api.weatherstack.com/forecast?query=${city}&units=m&access_key=${apiKey}`;
+//     const url = `https://api.weatherstack.com/forecast?query=${city}&units=m&access_key=${apiKey}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+//     const response = await fetch(url);
+//     const data = await response.json();
 
-    document.getElementById("weather").innerHTML = `
-    <h2>${data.location.name}</h2>
-    <p>${data.location.country}</p>
-    <p>${data.current.temperature}</p>
-    `;
-}
+//     document.getElementById("weather").innerHTML = `
+//     <h2>${data.location.name}</h2>
+//     <p>${data.location.country}</p>
+//     <p>${data.current.temperature}</p>
+//     `;
+// }
