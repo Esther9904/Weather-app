703e0348513e8cc23778636551b7f5560d19fdae
Esther9904 <taiwoibeh@gmail.com>
2026-07-10 17:22:07 +0100

    Made forecast dynamic

--- WeatherApi.js
+++ WeatherApi.js
@@ -1,8 +1,41 @@
 const temperature = document.querySelector(".temperature");
 const condition = document.querySelector(".condition");
+const weather = document.querySelector(".weather-icon");
+const forecastContainer = document.querySelector(".forecast")
+const weatherDescriptions = {
+    0: "Clear Sky",
+    1: "Mainly Clear",
+    2: "Partly Cloudy",
+    3: "Overcast",
+    45: "Fog",
+    48: "Depositing Rime Fog",
+    61: "Slight Rain",
+    63: "Moderate Rain",
+    65: "Heavy Rain",
+    80: "Rain Showers",
+    95: "Thunderstorm"
+};
+
+const weatherIcons = {
+    0: "sun",
+    1: "sun-medium",
+    2: "cloud-sun",
+    3: "cloud",
+    45: "cloud-fog",
+    48: "cloud-fog",
+    61: "cloud-drizzle",
+    63: "cloud-rain",
+    65: "cloud-rain",
+    80: "cloud-rain",
+    95: "cloud-lightning"
+}
+
+
 
 setTimeout(() => {
     temperature.classList.add("fade-out");
+    condition.classList.add("fade-out");
+    weather.classList.add("fade-out")
 }, 2000);
 
 // setTimeout(() => {
@@ -36,44 +69,38 @@ async function fetchWeather(latitude,longitude) {
 
 function updateWeatherUI(data) {
     console.log(data);
+    const code = data.current.weather_code;
+    const iconName = weatherIcons[code] || "cloud";
     setTimeout(() => {
         temperature.textContent = `${Math.round(data.current.temperature_2m)}°C`;
         temperature.classList.remove("fade-out");
+        
+        condition.textContent = weatherDescriptions[code] || "Unknown Condition";
+        condition.classList.remove("fade-out");
+
+        document.querySelector(".weather-icon").innerHTML =`<i data-lucide="${iconName}"></i>`;
+        lucide.createIcons();
+        weather.classList.remove("fade-out");
     }, 2500);
 
-    const code = data.current.weather_code;
+    forecastContainer.innerHTML = "";
 
-    let iconName = "cloud"
+    for (let i = 0; i <5; i++) {
+        const date = new Date(data.daily.time[i]);
+        
+        const day = date.toLocaleDateString("en-US", {
+            weekday: "short"
+        });
 
-    if(code === 0) {
-        iconName = "sun";
-    } else if(code === 1) {
-        iconName = "sun-medium";
-    } else if(code === 2) {
-        iconName = "cloud-sun";
-    } else if(code === 3) {
-        iconName = "cloud"
-    }else if(code === 80) {
-        iconName = " cloud-rain"
-    } else if(code === 95) {
-        iconName = "cloud-lightning";
+        const maxTemp = Math.round(data.daily.temperature_2m_max[i]);
+
+        forecastContainer.innerHTML += `
+            <div class= "forecast-card">
+                <h3>${day}</h3>
+                <i data-lucide="cloud"></i>
+                <p>${maxTemp}°</p>
+            </div>
+        `;
     }
-
-    document.querySelector(".weather-icon").innerHTML =`<i data-lucide="${iconName}"></i>`;
     lucide.createIcons();
-
-    if(code === 0) {
-        condition.textContent = "Clear Sky";
-    } else if (code === 1) {
-        condition.textContent = "Mainly Clear";
-    } else if (code === 2) {
-        condition.textContent = "Partly cloudy";
-    } else if (code === 3) {
-        condition.textContent = "Overcast";
-    } else if (code === 80) {
-        condition.textContent = "Rain showers";
-    } else {
-        condition.textContent = "Unknown weather";
-    }
-
 }
